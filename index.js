7b74d09c7515edf7c7d4fc0fd78b2d4d9c2456a6
simtor <simone.ram@ada.ac.uk>
2020-10-12 16:13:13 +0100

    Finished phase 1

--- index.js
+++ index.js
@@ -5,17 +5,38 @@ const rl = readline.createInterface({
   });
 
 
-
+ 
 let recursiveAsyncReadLine = function () {
   rl.question('Names of the drivers: ', function (answer) {
-    var words = answer.split(" ");
-    for (var i = 0; i < words.length - 1; i++) {
+    let chars = answer.split("");
+    for (let i = 0; i < chars.length - 1; i++) {
+        chars[i] += " ";
+    }
+    let words = answer.split(" ");
+    for (let i = 0; i < words.length - 1; i++) {
         words[i] += " ";
     }
+    if(!answer.includes(' ')){
+        return answer.length;
+     };
+     const { length: strLen } = answer;
+     const { length: numWords } = answer.split(' ');
+     const average = (strLen - numWords + 1) / numWords;
+     let findDuplicates = arr => arr.filter((item, index) => arr.lastIndexOf(item) != index)
+    
+     console.log(findDuplicates(words)) // All duplicates
+     console.log([...new Set(findDuplicates(words))]) // Unique duplicates
+
+    console.log("You have", words.length,"words in this sentence");
     console.log(words);
-    // if (answer == 'exit') 
-    //   return rl.close();
-    recursiveAsyncReadLine(); 
+    console.log("You have", chars.length,"chacters in this sentence");
+    console.log(chars);
+    console.log(average);
+    rl.close();
   });
 };
 recursiveAsyncReadLine();
+
+
+
+
