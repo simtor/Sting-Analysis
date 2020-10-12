4b08915a86ee51120db528cf68f500ed6949bf58
simtor <simone.ram@ada.ac.uk>
2020-10-12 13:58:23 +0100

    Alllowing word to be stored in list

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Simone Ram")
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin, 
+    output : process.stdout
+  });
+
+
+
+let recursiveAsyncReadLine = function () {
+  rl.question('Names of the drivers: ', function (answer) {
+    var words = answer.split(" ");
+    for (var i = 0; i < words.length - 1; i++) {
+        words[i] += " ";
+    }
+    console.log(words);
+    // if (answer == 'exit') 
+    //   return rl.close();
+    recursiveAsyncReadLine(); 
+  });
+};
+recursiveAsyncReadLine();
