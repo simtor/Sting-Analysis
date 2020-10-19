40b4ab14e81c2170df54c6bcce58c52508671856
simtor <simone.ram@ada.ac.uk>
2020-10-19 16:23:23 +0100

    testing formula 2

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const rl = readline.createInterface({
     output : process.stdout
   });
 
-
  
 let recursiveAsyncReadLine = function () {
   rl.question('PLease enter your text here: ', function (answer) {
@@ -21,23 +20,20 @@ let recursiveAsyncReadLine = function () {
     if(!answer.includes(' ')){
         return answer.length;
      };
+     
      answer = answer.toLowerCase();                                    
     if(answer.length <= 3) { return 1; }                             
       answer = answer.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   
       answer = answer.replace(/^y/, '');                                 
-     
-     
     let syllable = answer.match(/[aeiouy]{1,2}/g).length;
+
+
     let charsLen = chars.length;
     //  console.log(charsLen);
     let wordsLen = words.length;
     //  console.log(wordsLen);
     const average = (charsLen - wordsLen + 1) / wordsLen;
     let findDuplicates = arr => arr.filter((item, index) => arr.lastIndexOf(item) != index)
-    // const text = answer;
-    // console.log(text);
-    // let numOfSentences = text.match(/[\w|)][.?!](\s|$)/g).length;
-    // console.log(numOfSentences); 
      console.log(syllable);
      console.log(findDuplicates(words)) // All duplicates
      console.log([...new Set(findDuplicates(words))]) // Unique duplicates
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const readline = require('readline');
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
 const rl = readline.createInterface({
     input : process.stdin, 
     output : process.stdout
@@ -8,11 +10,21 @@ const rl = readline.createInterface({
 
 let recursiveAsyncReadLine = function () {
     rl.question('Names of the drivers: ', function (answer) {
+        let chars = answer.split("");
+        for (let i = 0; i < chars.length - 1; i++) {
+            chars[i] += " ";
+        }
+        let words = answer.split(" ");
+        for (let i = 0; i < words.length - 1; i++) {
+            words[i] += " ";
+        }
+        if(!answer.includes(' ')){
+            return answer.length;
+        };
         function calculateFlesch(totalSentences, totalWords, totalSyllables, ) {
             // return (206.835 - (1.015 * (totalWords/totalSentences)) - (84.6 *(totalSyllables/totalWords)));
             return (0.39 * (totalWords/totalSentences) + 11.8 * (totalSyllables/totalWords)-15.59)
           }
-          
           function getSyllables(word) {
             word = word.toLowerCase();
             if(word.length <= 3) { return 1; }
@@ -45,9 +57,25 @@ let recursiveAsyncReadLine = function () {
                 word = ""
               }
             });
+            let charsLen = chars.length;
+            //  console.log(charsLen);
+            let wordsLen = words.length;
+            //  console.log(wordsLen);
+            const average = (charsLen - wordsLen + 1) / wordsLen;
+            let findDuplicates = arr => arr.filter((item, index) => arr.lastIndexOf(item) != index)
+            console.log(findDuplicates(words)) // All duplicates
+            console.log([...new Set(findDuplicates(words))]) // Unique duplicates
+
+            console.log("You have", wordsLen,"words in this sentence");
+            console.log(words);
+            console.log("You have", charsLen,"chacters in this sentence");
+            console.log(chars);
+            console.log(average);
+            var result = sentiment.analyze(answer);
+            console.dir(result);
             console.log(totalSyllables);
             console.log(calculateFlesch(totalSentences, totalWords, totalSyllables));
-        
+            rl.close();
     });
   };
   recursiveAsyncReadLine();
